4933280a4dbee1238c448ea99e4cf4858477a982
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-10-14 15:54:22 +0100

    add Mesurment class

---
+++ Measurement.py
@@ -0,0 +1,11 @@
+class Measurement(object):
+	"""docstring for Measurement"""
+	def __init__(self,year,value,timeperiodId,timeperiodDescr):
+		self.year = year
+		self.value = value
+		self.timeperiodId = timeperiodId
+		self.timeperiodDescr = timeperiodDescr
+		
+
+		
+		
