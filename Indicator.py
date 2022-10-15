e6b202cfd6d6705b50823b8048690efa6aefe3b5
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-10-16 00:53:24 +0100

    fix Indicator Class

--- Indicator.py
+++ Indicator.py
@@ -1,8 +1,10 @@
 from IndicatorGroup import IndicatorGroup
+from Unit import Unit
 
 class Indicator(object):
-	def __init__(self,id:str,frequency:int,frequencyDesc:str,geoLocation:str,indicatorGroup:IndicatorGroup):
+	def __init__(self,id:str,frequency:int,frequencyDesc:str,geoLocation:str,indicatorGroup:IndicatorGroup, unit:Unit):
 		self.id = id 
+		self.unit = unit
 		self.indicatorGroup = indicatorGroup
 		self.__frequency = frequency
 		self.__frequencyDesc = frequencyDesc
