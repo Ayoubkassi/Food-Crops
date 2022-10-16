90d15005f895cdb3d905b73db4cac2b9a45404ba
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-10-16 14:43:15 +0100

    improve factoryDataset

--- FoodCropFactory.py
+++ FoodCropFactory.py
@@ -1,9 +1,12 @@
 from CommodityGroup import CommodityGroup
+from IndicatorGroup import IndicatorGroup
+from Measurement import Measurement
 from Commodity import Commodity
-from Volume import Volume
-from Price import Price
-from Weight import Weight
+from Indicator import Indicator
 from Surface import Surface
+from Volume import Volume
+from Weight import Weight
+from Price import Price
 from Count import Count
 from Ratio import Ratio
 from Unit import Unit
@@ -38,6 +41,15 @@ class FoodCropFactory(object):
 	@staticmethod
 	def createCommodity(group:CommodityGroup,id:int,name:str):
 		return Commodity(group,id,name)
+
+	@staticmethod
+	def createIndicator(id:int,frequency:int,freqDesc:str,geoLocation:str,indicatorGroup:IndicatorGroup,unit:Unit):
+		return Indicator(id,frequency,freqDesc,geoLocation,indicatorGroup,unit)
+
+	@staticmethod
+	def createMeasurement(id:int,year:int,value:float,timeperiodId:int,timeperiodDescr:str,commodity:Commodity,indicator:Indicator):
+		return Measurement(id,year,value,timeperiodId,timeperiodDescr,commodity,indicator)
+
 
 
 
